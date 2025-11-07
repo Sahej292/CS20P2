5da5ba7139fb1e4f6993eea0af653b180b9d4761
410029506 <410029506@L815WD220007.edu.cbe.ab.ca>
2025-11-07 10:00:06 -0700

    10:00am

--- Chapter7/src/SkillBuilders/student.java
+++ Chapter7/src/SkillBuilders/student.java
@@ -2,4 +2,20 @@ package SkillBuilders;
 
 public class student {
 
+	private String firstName;
+	private String lastName;
+	private String address;
+	private int StuID;
+	
+	// the same as a class
+	public student () //constructor method 
+	{
+	
+		firstName = "";
+		lastName = "";
+		address = "####";
+		StuID = 00000;
+	}
+
+
 }
