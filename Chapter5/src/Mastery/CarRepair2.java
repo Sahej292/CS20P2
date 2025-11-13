1b4a9d10e2229fccf06eb04aa481de93d50d590b
410029506 <410029506@L815WD220007.edu.cbe.ab.ca>
2025-11-13 09:03:22 -0700

    9:03 am

--- Chapter5/src/Mastery/CarRepair2.java
+++ Chapter5/src/Mastery/CarRepair2.java
@@ -8,25 +8,17 @@ public class CarRepair2 {
 		// TODO Auto-generated method stub
 		 // Create a Scanner object to get input from the user
         Scanner input = new Scanner(System.in);  
+       int carmodel; 
+      
+        
+        do {
+        	 System.out.print("Enter your car model (Enter 0 to quit):  ");
+        	 carmodel = input.nextInt();
+        } while (carmodel != 0);
        
-        // Ask the user to enter their car model
-        System.out.print("Enter your car model (Enter 0 to quit):  ");
-        
-        int carmodel = input.nextInt();
-        
-        if (carmodel == 0) {
-             
-          }
-        else if (carmodel == 119 || carmodel == 179 || carmodel == 189 || carmodel == 195 || carmodel == 221 || carmodel == 780) {
-                // If the car model matches any defective model, print this message
-                System.out.print("Your car is defective! It must be repaired.");
-            } else {
-                // If the car model does not match any defective model, print this message
-                System.out.print("Your car is not defective");
+     
             }
-
-        
-       
-	}
-
 }
+
+
+
