27d448f9bd3d265072bd763d5ceeee572a3eab0d
410029506 <410029506@L815WD220007.edu.cbe.ab.ca>
2026-01-05 11:57:31 -0700

    11:57am

---
+++ Phidgets/src/Level1/BlinkLED.java
@@ -0,0 +1,33 @@
+package Level1;
+
+import com.phidget22.*;
+
+public class BlinkLED {
+
+	public static void main(String[] args) throws Exception {
+		 DigitalOutput greenLED = new DigitalOutput();
+		 DigitalOutput redLED = new DigitalOutput();
+
+	        //Address 
+	        greenLED.setHubPort(5);
+	        greenLED.setIsHubPortDevice(true);
+	        redLED.setHubPort(1);
+	        redLED.setIsHubPortDevice(true);
+	        //Open 
+	        greenLED.open(1000);
+	        redLED.open(1000);
+
+	        //Use your Phidgets 
+	        while(true){
+	            greenLED.setState(true);
+	            Thread.sleep(2000);
+	            greenLED.setState(false);
+	            Thread.sleep(1000);
+	            
+	            redLED.setState(true);
+	            Thread.sleep(2000);
+	            redLED.setState(false);
+	            Thread.sleep(1000);
+	}
+	}
+}
